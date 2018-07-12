05f4100d4ef71fbc37c8560d3e36ce7972f4877d
Guillermo Cordeiro <gcordeiro@librebit.com>
2018-07-12 00:59:50 +0000

    Se elimina la variable history_limit

--- scripts/save_complete_history.sh
+++ scripts/save_complete_history.sh
@@ -8,9 +8,7 @@ source "$CURRENT_DIR/shared.sh"
 main() {
 	if supported_tmux_version_ok; then
 		local file
-		local history_limit
 		file=$(expand_tmux_format_path "${save_complete_history_full_filename}")
-		history_limit="$(tmux display-message -p -F "#{history_limit}")"
 		tmux capture-pane -J -S "-" -p -e > "${file}"
 		remove_empty_lines_from_end_of_file "${file}"
 		display_message "History saved to ${file}"
